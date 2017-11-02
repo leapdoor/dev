51e156b3891520b602edcc6f21dd83cf29be5cda
Lanka <lanka@minthrm.com>
2017-11-02 15:35:43 +0530

    add some files

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,15 +1,18 @@
 <?php
 
+// This is the database connection configuration.
 return array(
     'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/testdrive.db',
-     
-    'connectionString' => 'mysql:host=localhost;dbname=dev',
-    
+    // uncomment the following lines to use a MySQL database
+
+//'connectionString' => 'mysql:host=192.168.1.14;dbname=srlc',
+//'connectionString' => 'mysql:host=192.168.43.109;dbname=srlc',
+    'connectionString' => 'mysql:host=localhost:3307;dbname=srlc',
     'emulatePrepare' => true,
-	
     'username' => 'root',
-    'password' => '123@sg',
+//	'password' => 'managerinfo1',
+    'password' => '',
+//    'password' => 'BattelShip',
     'charset' => 'utf8',
+
 );
-
-
