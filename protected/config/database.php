baf33859409decbc810af0309ada9136f2243d18
sanjeewa <sanjeewa@infoseek.lk>
2017-11-08 07:06:24 +0530

    fixed issue

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,14 +5,10 @@ return array(
     'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/testdrive.db',
     // uncomment the following lines to use a MySQL database
 
-//'connectionString' => 'mysql:host=192.168.1.14;dbname=srlc',
-//'connectionString' => 'mysql:host=192.168.43.109;dbname=srlc',
-    'connectionString' => 'mysql:host=localhost:3307;dbname=srlc',
+    'connectionString' => 'mysql:host=localhost;dbname=dev',
     'emulatePrepare' => true,
     'username' => 'root',
-//	'password' => 'managerinfo1',
     'password' => '',
-//    'password' => 'BattelShip',
     'charset' => 'utf8',
 
 );
